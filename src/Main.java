1dc76fcd582b05789f10e85602fe532566d16996
soobinhand <chelsea9528@naver.com>
2021-06-20 17:15:19 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        UF a = new UF(1);
+        a.union(1,3);
+        a.connected(1,3);
+        a.count();
+
+
     }
 }
