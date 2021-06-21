e6eb3985005f38f7d5e063406a5572421f088039
soobinhand <chelsea9528@naver.com>
2021-06-21 18:51:48 +0900

    a

---
+++ src/baekjoon/B5/_10718.java
@@ -0,0 +1,9 @@
+package baekjoon.B5;
+
+public class _10718 {
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군");
+
+    }
+}
---
+++ src/programmers/LV1/wanjoomothan.java
@@ -0,0 +1,16 @@
+package programmers.LV1;
+
+import java.util.Arrays;
+
+public class wanjoomothan {
+    public String solution(String[] participant, String[] completion){
+        String answer = "";
+        String temp = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        return answer;
+
+    }
+}
