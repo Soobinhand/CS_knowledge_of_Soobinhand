2ef7883f27e7368edb386d9eda352976ae9152c1
soobinhand <chelsea9528@naver.com>
2021-06-23 01:06:52 +0900

    a

---
+++ src/programmers/LV1_impl/eumyangplus_impl.java
@@ -0,0 +1,12 @@
+package programmers.LV1_impl;
+
+import programmers.LV1.eumyangplus;
+
+public class eumyangplus_impl {
+    public static void main(String[] args){
+        eumyangplus a = new eumyangplus();
+        int[] b = {4,7,12};
+        boolean[] c = {true,false,true};
+        System.out.println(a.solution(b,c));
+    }
+}
