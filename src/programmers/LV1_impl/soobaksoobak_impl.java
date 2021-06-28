eada4847af841b4b449ffc666097bd6c95825c31
soobinhand <chelsea9528@naver.com>
2021-06-28 16:59:59 +0900

    a

---
+++ src/programmers/LV1_impl/soobaksoobak_impl.java
@@ -0,0 +1,11 @@
+package programmers.LV1_impl;
+
+import programmers.LV1.soobaksoobak;
+
+public class soobaksoobak_impl {
+    public static void main(String[] args){
+        int a = 1;
+        soobaksoobak ss = new soobaksoobak();
+        System.out.println(ss.solution(a));
+    }
+}
