37a715390b638e72403b2b4bb48bdef74330a8c9
soobinhand <chelsea9528@naver.com>
2021-07-02 21:25:30 +0900

    a

---
+++ src/baekjoon/B1/_2748.java
@@ -0,0 +1,24 @@
+package baekjoon.B1;
+
+import java.util.Scanner;
+
+public class _2748 {
+
+    static long[] memo;
+
+    public static void main(String[] args){
+        memo = new long[101];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(F(n));
+    }
+
+    public static long F(int n){
+        if(n<=1)
+            return n;
+        else if(memo[n]!=0)
+            return memo[n];
+        else
+            return memo[n] = F(n-1)+F(n-2);
+    }
+}
