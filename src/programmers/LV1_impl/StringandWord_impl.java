9e39ced0eb04f88071104e115fba36212387c17e
soobinhand <chelsea9528@naver.com>
2021-07-31 01:17:49 +0900

    a

---
+++ src/programmers/LV1_impl/StringandWord_impl.java
@@ -0,0 +1,10 @@
+package programmers.LV1_impl;
+
+import programmers.LV1.StringandWord;
+
+public class StringandWord_impl {
+    public static void main(String[] args) {
+        StringandWord sw = new StringandWord();
+        System.out.println(sw.solution("one9ninefive"));
+    }
+}
