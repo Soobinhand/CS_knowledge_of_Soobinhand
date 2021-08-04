d425cfd18dc0ce3411ff3982331d673708f80b33
soobinhand <chelsea9528@naver.com>
2021-08-05 01:13:26 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-public class Main {
+import java.util.PriorityQueue;
 
-    public static void main(String[] args) {
-        int a = 10;
-        int b =10;
-        int c = 10;
+public class Main{
 
-    }
 }
