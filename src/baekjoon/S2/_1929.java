5fba8446ce2a81639cee1934ee694d0ecef1aeea
soobinhand <chelsea9528@naver.com>
2021-08-09 00:15:09 +0900

    a

--- src/baekjoon/S2/_1929.java
+++ src/baekjoon/S2/_1929.java
@@ -3,26 +3,30 @@ package baekjoon.S2;
 import java.util.Scanner;
 
 public class _1929 {
+    public static boolean[] prime;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int m = sc.nextInt();
         int n = sc.nextInt();
-        boolean[] res = new boolean[n+1];
-        for(int i=2;i<=n;i++){
-            res[i] = true;//일단 다 소수라고 두는 것임.
-        }
-        for(int i=2;i<Math.sqrt(n);i++){
-            if(res[i]){
-                for(int j=2;i*j<=n;j++){
-                    res[i*j] = false;
-                }
-            }
-        }//이 식이 소수 구하는 식
-        for(int i=m;i<=n;i++){
-            if (res[i])
-                System.out.println(i);
-        }
 
+        prime = new boolean[n+1];
+        get_prime();
+        for(int i=m;i<=n;i++)
+            if (!prime[i])
+                System.out.println(i);
+
+
+    }
+
+    //소수구하는 에라토스테네스의 체
+   public static void get_prime(){
+        prime[0] = prime[1] = true;
+        for(int i = 2;i<Math.sqrt(prime.length);i++){
+            if(prime[i]) continue;
+            for(int j=i*i;j<prime.length;j+=i){
+                prime[j] = true;
+            }
+        }
     }
 }
