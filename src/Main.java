ba77cd4e02f1809644e86e52eeb82adc1223d80e
soobinhand <chelsea9528@naver.com>
2021-08-14 03:50:36 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+
+import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+    static int f(int a){
+        if (a==0) return 0;
+        if (a==1) return 1;
+        else{
+            return f(a-1)+f(a-2);
+        }
 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(f(sc.nextInt())%10009);
+
+
+    }
 }
