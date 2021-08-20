d98eae0edd065e8fb7e54cf767be32a90c5872c2
soobinhand <chelsea9528@naver.com>
2021-08-20 23:56:51 +0900

    a

--- src/baekjoon/DP/B2_10870.java
+++ src/baekjoon/DP/B2_10870.java
@@ -7,7 +7,6 @@ public class B2_10870 {
 
     public static int f(int n){
         if(n<=1) return n;
-        else if(memo[n]!=0) return memo[n];
         return memo[n] = f(n-1)+f(n-2);
     }
     public static void main(String[] args){
